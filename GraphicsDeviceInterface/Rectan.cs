8f9e9a3cf6ceb01938f436a7c6b4510576e40b10
Anas Alhmoud <anas.alhmoud@tuwaiq.edu.sa>
2021-04-28 00:14:58 +0300

    Add basic Rectangle class

---
+++ GraphicsDeviceInterface/Rectan.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace GraphicsDeviceInterface
+{
+    public class Rectan: Shape
+    {
+        public Rectan(int x1, int y1)
+        {
+            this.x1 = x1;
+            this.y1 = y1;
+            this.type = ShapeType.Rectangle;
+            pen = new Pen(Color.Black, 5);
+        }
+        public override void Draw(Graphics g, bool preview = false)
+        {
+           if (preview)
+                g.DrawRectangle(new Pen(Color.DarkGray, 3), x1, y1, Width, Height);
+           else
+                g.DrawRectangle(this.pen, x1, y1, Width, Height);
+
+        }
+    }
+}
